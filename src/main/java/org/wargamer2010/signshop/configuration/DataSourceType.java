fba22c390bd02a2f1cf0c8de8e120b5b43a50d92
Matthew Meredith <matthewmeredith775@gmail.com>
2022-09-06 00:21:01 -0400

    Simplify checking if a DataSourceType points to an guaranteed internal source, or a potentially unavailable external source

--- src/main/java/org/wargamer2010/signshop/configuration/DataSourceType.java
+++ src/main/java/org/wargamer2010/signshop/configuration/DataSourceType.java
@@ -1,16 +1,23 @@
 package org.wargamer2010.signshop.configuration;
 
+import org.wargamer2010.signshop.util.SignShopLogger;
+
 public enum DataSourceType {
-    YML("yml", "org.sqlite.SQLiteDataSource"),
-    SQLITE("sqlite", "org.sqlite.SQLiteDataSource"),
-    MARIADB("mariadb", "org.mariadb.jdbc.MariaDbDataSource"),
-    MYSQL("mysql", "com.mysql.cj.jdbc.MysqlDataSource");
+    YML("yml", false, "org.sqlite.SQLiteDataSource"),
+    SQLITE("sqlite", false, "org.sqlite.SQLiteDataSource"),
+    MARIADB("mariadb", true, "org.mariadb.jdbc.MariaDbDataSource"),
+    MYSQL("mysql", true, "com.mysql.cj.jdbc.MysqlDataSource");
 
     private final String jdbcName;
+    private final boolean external;
     private final String dataSourceClass;
 
     public String getJdbcName() {
         return jdbcName;
+    }
+
+    public boolean isExternal() {
+        return external;
     }
 
     public String getDataSourceClass() {
@@ -18,17 +25,20 @@ public enum DataSourceType {
     }
 
     public static DataSourceType fromConfigName(String configName) {
-        for (DataSourceType type : .values()){
+        for (DataSourceType type : DataSourceType.values()) {
             if (type.getJdbcName().equalsIgnoreCase(configName)) {
                 return type;
             }
         }
 
+        SignShopLogger.getConfigLogger().error(configName + " is not a valid DataSourceType");
+
         return null;
     }
 
-    DataSourceType(String jdbcName, String dataSourceClass) {
+    DataSourceType(String jdbcName, boolean external, String dataSourceClass) {
         this.jdbcName = jdbcName;
+        this.external = external;
         this.dataSourceClass = dataSourceClass;
     }
 
